462d66e336bd0fd6f2e8ec5a9a9810e31c43a11d
mor biton <mor0981@gmail.com>
2020-04-06 21:33:47 +0300

    Test2

--- test2.py
+++ test2.py
@@ -1,13 +1,36 @@
 import unittest
+import pyrebase
 
+config={
+  "apiKey": "AIzaSyDab7tKKm11tgRuLsAPejXGGAYJ1d20cnQ",
+  "authDomain": "parkflask.firebaseapp.com",
+  "databaseURL": "https://parkflask.firebaseio.com",
+  "projectId": "parkflask",
+  "storageBucket": "parkflask.appspot.com",
+  "messagingSenderId": "685599054335",
+  "appId": "1:685599054335:web:db2d1d2890588a14772fca",
+  "measurementId": "G-H8HGMEE4WB"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth= firebase.auth()
 
 class TestHello(unittest.TestCase):
-    def test_setUp(self):
-        self.assertTrue(True)
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-    def test_upper3(self):
-        self.assertEqual('fodo'.upper(), 'FODO')
+    #User login with correct details
+    def test_correct(self):
+        try:
+            auth.sign_in_with_email_and_password("mor0981@gmail.com","12661266")
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+    #User login with incorrect details
+    def test_incorrect(self):
+        try:
+            auth.sign_in_with_email_and_password("mor081@gmail.com","12661266")
+            self.assertTrue(False)
+        except:
+            self.assertTrue(True)
+    
 
 
 if __name__ == '__main__':
