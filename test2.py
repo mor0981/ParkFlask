348bfc77a43c4e74fc03c885bbcfb1480d0a3c76
mor biton <mor0981@gmail.com>
2020-04-06 20:37:32 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,6 +6,8 @@ class TestHello(unittest.TestCase):
         self.assertTrue(True)
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+    def test_upper3(self):
+        self.assertEqual('fodo'.upper(), 'FODO')
 
 
 if __name__ == '__main__':
