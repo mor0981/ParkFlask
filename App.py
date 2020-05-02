b8d7575c78d65252a0465d6440ef5ad83efd68bb
mor biton <mor0981@gmail.com>
2020-05-02 17:48:35 +0300

    PM2020T5-66 Page Design

--- App.py
+++ App.py
@@ -11,10 +11,6 @@ app.config['SECRET_KEY']='mormormor'
 
 # default_app = firebase_admin.initialize_app()
 
-cred = credentials.Certificate('C:\\Users\\Moran Lavi\\Documents\\GitHub\\ParkFlask\\parkflask-firebase-adminsdk-wplsp-78f1875805.json')
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
 
 
 cred = credentials.Certificate('C:\\Users\\mor09\\Desktop\\scholl\\Parck\\parkflask-firebase-adminsdk-wplsp-87a9bb6106.json')
@@ -44,7 +40,13 @@ def home():
     if form.validate_on_submit():
         try:
             user=auth.sign_in_with_email_and_password(form.email.data,form.password.data)
-            print(auth.get_account_info(user['idToken'])['users'][0]['localId'])
+            uid=auth.get_account_info(user['idToken'])['users'][0]['localId']
+            doc_ref=db.collection(u"Users").doc(u str(uid))
+            doc = doc_ref.get()
+            if doc.exists:
+                print(u'Document data: {}'.format(doc.to_dict()))
+            else:
+                print(u'No such document!')
             session["user"]=form.email.data
             return redirect(url_for("user"))
         except:
@@ -53,6 +55,12 @@ def home():
         if "user" in session:
             return redirect(url_for("user"))
         return render_template('index.html',form=form)
+
+
+@app.route('/homePage',methods=['GET', 'POST'])
+def home():
+    
+        return render_template('homePage.html',form=form)
 
 @app.route('/user',methods=['GET', 'POST'])
 def user():
@@ -69,56 +77,56 @@ def logout():
     session.pop("user",None)
     return redirect(url_for("home")
 
-@app.route('/newpark', methods =['GET','POST'])
-def newpark():
 
-    form = NewParkForm()
-    if form.validate_on_submit():
+# @app.route('/newpark', methods =['GET','POST'])
+# def newpark():
+#     form = NewParkForm()
+#     if form.validate_on_submit():
 
-        data = {
-        "name": form.parkName.data,
-        "other": form.parkAddress.data,
-        "shadowing": form.shadow.data
-        }
-        docs = db.collection(u'Parks').stream()
-        canMakePark = True
-        for doc in docs:
-            dici = doc.to_dict()
-            if data["name"] == dici['name'] and data["other"] == dici['other']:
-                canMakePark = False
+#         data = {
+#         "name": form.parkName.data,
+#         "other": form.parkAddress.data,
+#         "shadowing": form.shadow.data
+#         }
+#         docs = db.collection(u'Parks').stream()
+#         canMakePark = True
+#         for doc in docs:
+#             dici = doc.to_dict()
+#             if data["name"] == dici['name'] and data["other"] == dici['other']:
+#                 canMakePark = False
 
-        if canMakePark:
-            db.collection(u'Parks').document().set(data)
-            flash(" יצרת פארק חדש ")
-        else:
-            flash("לא ניתן ליצור פארק")
+#         if canMakePark:
+#             db.collection(u'Parks').document().set(data)
+#             flash(" יצרת פארק חדש ")
+#         else:
+#             flash("לא ניתן ליצור פארק")
 
-
-        return redirect(url_for('newpark'))
-    return render_template('createNewPark.html', form=form)
-
-
-@app.route('/deletepark', methods =['GET','POST'])
-def deletepark():
-
-    form = DeleteParkForm()
-    if form.validate_on_submit():
-
-        req = request.form
-        parkName = req["parkName"]
-        parkAddress = req["parkAddress"]
-
-        docs = db.collection(u'Parks').stream()
-        for doc in docs:
-            dici = doc.to_dict()
-            if parkName == dici['name'] and parkAddress == dici['other']:
-                print (f"park {dici['name']} in {dici['other']} has beem deleted")
-                db.collection(u'Parks').document(doc.id).delete()
-                flash("מחקת פארק")
+#         return redirect(url_for('newpark'))
+#     return render_template('createNewPark.html', form=form)
 
 
-        return redirect(url_for('deletepark'))
-    return render_template('deletePark.html', form=form)
+# @app.route('/deletepark', methods =['GET','POST'])
+# def deletepark():
+#     form = DeleteParkForm()
+#     if form.validate_on_submit():
+
+#         req = request.form
+#         parkName = req["parkName"]
+#         parkAddress = req["parkAddress"]
+
+#         docs = db.collection(u'Parks').stream()
+#         for doc in docs:
+#             dici = doc.to_dict()
+#             if parkName == dici['name'] and parkAddress == dici['other']:
+#                 print (f"park {dici['name']} in {dici['other']} has beem deleted")
+#                 db.collection(u'Parks').document(doc.id).delete()
+#                 flash("מחקת פארק")
+
+
+#         return redirect(url_for('deletepark'))
+#     return render_template('deletePark.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
