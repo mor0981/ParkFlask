e2d48593b024927f08302bb66314f020c7701006
ran232003 <45311381+ran232003@users.noreply.github.com>
2020-05-18 10:25:52 +0300

    PM2020T5-74

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField , SubmitField , RadioField
+from wtforms import StringField, PasswordField , SubmitField , RadioField,TextAreaField,TextField
 from wtforms.validators import DataRequired , Length , Email
 
 class LoginForm(FlaskForm):
@@ -44,3 +44,9 @@ class signout2Form(FlaskForm):
     password= PasswordField("סיסמא")
     username = StringField("שם משתמש")
     submit=SubmitField("ביטול מנוי")
+
+class commentForm(FlaskForm):
+    email = StringField("דואר אלקטרוני")
+    password= PasswordField("סיסמא")
+    submit=SubmitField("שלח")
+    comment=TextAreaField("תגובה")
